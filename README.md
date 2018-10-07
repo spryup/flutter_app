b1543f4a50edac095e731aa89d2c60830383bd6c
amritbhanu <amritbhanu@gmail.com>
2018-10-06 21:21:11 -0400

    Update maps code

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ A new Flutter application.
 
 For help getting started with Flutter, view our online
 [documentation](https://flutter.io/).
+
+
+## Initialize Android Maps Secret key as hidden file
+
+- Create lib/keys.dart file, and write ```const googleMapsKey = 'Maps-Key';```
+- Create android/app/src/main/res/values/secrets.xml file and write
+    ```<?xml version="1.0" encoding="utf-8"?>
+       <resources>
+           <string name="google_maps_api_key">Maps-key</string>
+       </resources>
+    ```
--- lib/mapPage.dart
+++ lib/mapPage.dart
@@ -1,39 +1,143 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'profilePage.dart';
 import 'inbox.dart';
 import 'main.dart';
 import 'profile.dart';
+import 'package:map_view/map_view.dart';
+import 'package:flutter_search_bar/flutter_search_bar.dart';
+import 'keys.dart';
+//import 'package:location/location.dart';
+//import 'package:flutter_map/flutter_map.dart';
+
+const API_KEY = googleMapsKey;
 
 class MapPage extends StatefulWidget {
+
   @override
   MapPageState createState() {
-    return new MapPageState();
+    //MapView.setApiKey(API_KEY);
+  return new MapPageState();
   }
 }
 
 class MapPageState extends State<MapPage> {
-  @override
+  MapView mapView;
+  SearchBar searchBar;
+  StaticMapProvider staticMapProvider;
+  Uri imageUri;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
+  var latitude = 35.766538;
+  var longitude = -78.673005;
+  var currentlocation = new Location(35.766538,-78.673005);
+
+
+  @override
   void initState() {
     super.initState();
-
+    MapView.setApiKey(API_KEY); // -----------------new
+    mapView = new MapView();
     fetchData();
-  }
-
-  fetchData() async {
-
+    //currentlocation;
 
   }
 
-  Widget build(BuildContext context) {
+  fetchData() async {}
+
+  AppBar buildAppBar(BuildContext context) {
+    return new AppBar(
+        title: new Text('Search'),
+        actions: [searchBar.getSearchAction(context)],
+        backgroundColor: Colors.orange,);
+  }
+
+  MapPageState() {
+    searchBar = new SearchBar(
+        inBar: false,
+        buildDefaultAppBar: buildAppBar,
+        setState: setState,
+        onSubmitted: onSubmitted);
+  }
+
+  void onSubmitted(String value) {
+    setState(() => _scaffoldKey.currentState
+        .showSnackBar(new SnackBar(content: new Text('You wrote $value!'))));
+  }
+
+  void _showMapView() {
+    mapView.show(
+        new MapOptions(
+            mapViewType: MapViewType.normal,
+            showUserLocation: true,
+            initialCameraPosition: new CameraPosition(currentlocation, 14.0),
+//            initialCameraPosition: new CameraPosition(
+//              // if user hasn't selected a location, we show current location and vice versa.
+//                selectedLocation != null ?
+//                new Location(selectedLocation.latitude, selectedLocation.longitude)
+//                    : new Location(currentLocation.latitude, currentLocation.longitude),
+//                14.0 // zoom level
+//            ),
+            title: "Your location"),
+        toolbarActions: [new ToolbarAction("Close", 1), new ToolbarAction("Confirm", 2)]);
+
+    mapView.onMapReady.listen((Null _){
+      mapView.setMarkers([new Marker("1", "selected",latitude, longitude)]);
+    });
+
+    mapView.onMapTapped.listen((location) {
+      print("tapped location is $location");
+      mapView.setMarkers([new Marker("1", "selected",latitude, longitude)]);
+      print(mapView.markers.length);
+    });
+
+    mapView.onToolbarAction.listen((id) {
+      if (id == 1) {
+        mapView.dismiss();
+      } else if (id == 2) {
+        print("len is: " + mapView.markers.length.toString());
+        if (mapView.markers.isNotEmpty){
+          setState(() {
+            imageUri = staticMapProvider.getStaticUriWithMarkers(mapView.markers);
+            currentlocation = Location(latitude, longitude);
+          });
+
+          mapView.dismiss();
+        }
+
+      }
+    });
+
+  }
+
+
+    Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        backgroundColor: Colors.orange,
-        title: new Text("Seach Map"),
-
+      appBar: searchBar.build(context),
+      key: _scaffoldKey,
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: new Center(
+                child: ClipRRect(
+                  child: new Image.network(imageUri.toString()),
+                  borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _showMapView,
+        tooltip: 'show map',
+        child: new Icon(Icons.map),
       ),
 
-      drawer: new Drawer(
+    drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
             new UserAccountsDrawerHeader(
@@ -86,7 +190,18 @@ class MapPageState extends State<MapPage> {
         ),
       ),
 
-
     );
   }
+
+//  Future<void> _setCurrentLocation() async {
+//    geo.Geolocation.currentLocation(accuracy: geo.LocationAccuracy.best)
+//        .listen((result) {
+//      if (result.isSuccessful) {
+//        setState(() {
+//          currentLocation = result.location;
+//        });
+//      }
+//    });
+//  }
+
 }
